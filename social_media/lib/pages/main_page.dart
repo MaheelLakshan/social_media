f6f60dce1dc84990d80c4600dbc46e13bc941e09
MaheelLakshan <MaheelLakshanra@gmail.com>
2024-03-13 12:24:01 +0530

    page shifting was done

--- social_media/lib/pages/main_page.dart
+++ social_media/lib/pages/main_page.dart
@@ -1,27 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:social_media/pages/home_page.dart';
+// import 'package:flutter_svg/svg.dart';
+import 'package:social_media/styles/app_colors.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Bottom Navigator Container'),
-      ),
-      body: const Center(
-        child: Text('something  on home page'),
-      ),
+      body: pages[currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              // icon: SvgPicture.asset('asserts/svg/home.svg'), label: 'Home'),
+              icon: Icon(Icons.home),
+              label: 'Home'),
           BottomNavigationBarItem(
               icon: Icon(Icons.favorite), label: 'Favorite'),
-          BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Add post'),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.add,
+            ),
+            label: 'Add post',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.message,
+            ),
+            label: 'Message',
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_box), label: 'Users'),
         ],
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
         showSelectedLabels: false,
         showUnselectedLabels: false,
+        backgroundColor: Colors.amber,
       ),
     );
   }
+
+  final pages = const [
+    HomePage(),
+    Center(child: const Text('Favorite')),
+    Center(child: const Text('Add Post')),
+    Center(child: const Text('Message')),
+    Center(child: const Text('Users')),
+  ];
 }
